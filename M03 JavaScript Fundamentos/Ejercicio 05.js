04fa5f74b7c3385bd7c0a38b3581291b21f676ff
Zafiro1996 <isaiasvelasquez1996@gmail.com>
2023-09-15 14:49:36 -0300

    Ejercicios-prepcourse

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -8,6 +8,8 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve false.
    // Tu código:
+ 
+   
 }
 
 function agregarSimboloExclamacion(str) {
